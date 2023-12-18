aea02f780c2c9d768fe2a0d031f82520b1ff1bca
Igor Zalutski <izalutski@gmail.com>
2023-12-18 12:36:46 +0000

    Initial commit

---
+++ bucket-only/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "bucket_id" {
+  type = string
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_id
+}
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
